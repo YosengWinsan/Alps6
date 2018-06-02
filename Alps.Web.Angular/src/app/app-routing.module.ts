993d3b55404640f624269ebb38be4c7bf57e7d38
YosengWinsan <YosengTech@qq.com>
2018-06-02 16:35:12 +0800

    测试

--- Alps.Web.Angular/src/app/app-routing.module.ts
+++ Alps.Web.Angular/src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from "./dashboard/dashboard.component";
 const routes: Routes = [
 {path:"",redirectTo:"dashboard",pathMatch:"full"},
-{path:"dashboard",component:DashboardComponent}
+{path:"dashboard",component:DashboardComponent},
+{path:"catagory",loadChildren:"./catagory/catagory.module#CatagoryModule"}
 ];
 
 @NgModule({
--- Alps.Web.Angular/src/app/catagory/catagory.module.ts
+++ Alps.Web.Angular/src/app/catagory/catagory.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CatagoryRoutingModule } from './catagory-routing.module';
+import { ListComponent } from './list/list.component';
+import { EditComponent } from './edit/edit.component';
 
 @NgModule({
   imports: [
     CommonModule,
     CatagoryRoutingModule
   ],
-  declarations: []
+  declarations: [ListComponent, EditComponent]
 })
 export class CatagoryModule { }
